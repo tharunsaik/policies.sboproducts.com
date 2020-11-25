1c049acb36efb494b6168ec9a4f001159cf64a5a
K Tharun Sai <tharunsaik@users.noreply.github.com>
2020-11-25 23:06:14 +0530

    index

---
+++ header.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+
+		<link rel="stylesheet" href="assets/css/svl-global.css">
+		<!-- UIkit CSS -->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.3.3/dist/css/uikit.min.css" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" />
+		<link href="https://cdn.lineicons.com/2.0/LineIcons.css" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="assets/css/back-to-top.css">
+		<link rel="stylesheet" href="assets/css/global.css">
+		<link rel="stylesheet" href="assets/css/styles.css">
+
+		<title>Policies</title>
+		<link rel="icon" href="assets/img/fav.png" type="image/gif">
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-light bg-white uk-box-shadow-small" uk-sticky="cls-active: navbarFixed; ">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="index.php">
+					<img src="assets/img/logo-svl-global.svg" alt="" width="170">
+				</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarMain">
+					<div class="navbar-nav ml-auto align-items-sm-center">
+						<a class="nav-item nav-link active" href="index.php">Overview</a>
+						<a class="nav-item nav-link" href="privacy-policy.php">Privacy Policy</a>
+						<a class="nav-item nav-link" href="privacy-policy.php">Terms of Service</a>
+						<a class="nav-item nav-link" href="privacy-policy.php">Technologies</a>
+						<a class="nav-item nav-link" href="privacy-policy.php">FAQ</a>
+						<div class="nav-item dropdown allServicesDropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="allServicesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								<i class="fas fa-th"></i>
+							</a>
+							<div class="dropdown-menu dropdown-menu-right" aria-labelledby="allServicesDropdown">
+								<ul class="nav text-center">
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -345px;"></span>
+											<span class="services-text">Account</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -138px;"></span>
+											<span class="services-text">Search</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -1035px;"></span>
+											<span class="services-text">Maps</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -1449px;"></span>
+											<span class="services-text">YouTube</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -759px;"></span>
+											<span class="services-text">Play</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -2208px;"></span>
+											<span class="services-text">News</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -2139px;"></span>
+											<span class="services-text">Gmail</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -2622px;"></span>
+											<span class="services-text">Contacts</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 0;"></span>
+											<span class="services-text">Drive</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -1794px;"></span>
+											<span class="services-text">Calendar</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -1587px;"></span>
+											<span class="services-text">Translate</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -2070px;"></span>
+											<span class="services-text">Photos</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -69px;"></span>
+											<span class="services-text">Shopping</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -1242px;"></span>
+											<span class="services-text">Duo</span>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
---
+++ index.php
@@ -0,0 +1,77 @@
+<!--
+Header Start
+-->
+<?php include 'header.php';?>
+<!--
+Header End
+-->
+<!--
+Main Content Start
+-->
+<div class="uk-container uk-container-small py-5">
+	<div class="row py-5">
+		<div class="col-sm">
+			<h3 class="Z6jTqd">Privacy Policy</h3>
+			<p>Explains what information we collect and why, how we use it, and how to review and update it.</p>
+			<p class="rfNEoc"><a href="privacy-policy.php">Read our Privacy Policy</a></p>
+		</div>
+		<div class="col-sm">
+			<h3 class="Z6jTqd">Terms of Service</h3>
+			<p>Describes the rules you agree to when using our services.</p>
+			<p class="rfNEoc"><a href="privacy-policy.php">Read our Terms of Service</a></p>
+		</div>
+	</div>
+	<div class="row py-5">
+		<div class="col-sm">
+			<h3 class="Z6jTqd">Google Safety Center</h3>
+			<div class="media">
+				<img alt="" class="mr-3" src="https://www.gstatic.com/policies/images/shields2.png" srcset="https://www.gstatic.com/policies/images/shields2_2x.png 2x" data-iml="3797.950000000128" data-atf="true">
+				<div class="media-body">
+					<p class="rfNEoc">Making products for everyone means protecting everyone who uses them. Visit safety.google to learn more about our built-in security, privacy controls, and tools to help set digital ground rules for your family online.</p>
+					<p class="rfNEoc"><a href="privacy-policy.php">Explore what we do to help keep you safe</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm">
+			<h3 class="Z6jTqd">Google Account</h3>
+			<div class="media">
+				<img alt="" class="mr-3" src="https://www.gstatic.com/policies/images/my_account.png" srcset="https://www.gstatic.com/policies/images/my_account_x2.png 2x" data-iml="3798.030000001745" data-atf="true">
+				<div class="media-body">
+					<p class="rfNEoc r0hqg">Control, protect, and secure your account, all in one place. Your Google Account gives you quick access to settings and tools that let you safeguard your data and protect your privacy.</p>
+					<p class="rfNEoc"><a href="privacy-policy.phpn">Visit your Google Account</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row py-5">
+		<div class="col-sm">
+			<h3 class="Z6jTqd">Our Privacy and Security Principles</h3>
+			<div class="media">
+				<img alt="" class="mr-3" src="https://www.gstatic.com/policies/images/privacy_security_answers.png" srcset="https://www.gstatic.com/policies/images/privacy_security_answers_x2.png 2x" data-iml="3798.140000002604" data-atf="false">
+				<div class="media-body">
+					<p class="rfNEoc">We build privacy that works for everyone. It’s a responsibility that comes with creating products and services that are free and accessible for all. We look to these principles to guide our products, our processes, and our people in keeping our users’ data private, safe, and secure.</p>
+					<p class="rfNEoc"><a href="privacy-policy.php">Explore our Privacy and Security Principles</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm">
+			<h3 class="Z6jTqd">Google Product Privacy Guide</h3>
+			<div class="media">
+				<img alt="" class="mr-3" src="https://www.gstatic.com/policies/images/product_privacy.png" srcset="https://www.gstatic.com/policies/images/product_privacy_x2.png 2x" data-iml="3798.210000000836" data-atf="false">
+				<div class="media-body">
+					<p class="rfNEoc r0hqg">As you use Gmail, Search, YouTube, and other products from Google, you have the power to control and protect your personal information and usage history. The <a href="privacy-policy.php">Google Product Privacy Guide</a> can help you find information about how to manage some of the privacy features built into Google's products.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--
+Main Content End
+-->
+<!--
+Footer Start
+-->
+<?php include 'footer.php';?>
+<!--
+Footer Start
+-->
