03cf8e3440a3140f5936724e3807c188bce2106d
K Tharun Sai <tharunsaik@users.noreply.github.com>
2021-02-27 12:44:39 +0530

    index

--- index.php
+++ index.php
@@ -1,7 +1,141 @@
 <!--
 Header Start
 -->
-<?php include 'header.php';?>
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+
+		<link rel="stylesheet" href="assets/css/svl-global.css">
+		<!-- UIkit CSS -->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.3.3/dist/css/uikit.min.css" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" />
+		<link href="https://cdn.lineicons.com/2.0/LineIcons.css" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="assets/css/back-to-top.css">
+		<link rel="stylesheet" href="assets/css/global.css">
+		<link rel="stylesheet" href="assets/css/styles.css">
+
+		<title>Policies</title>
+		<link rel="icon" href="assets/img/fav.png" type="image/gif">
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-light bg-white uk-box-shadow-small" uk-sticky="cls-active: navbarFixed; ">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="index.php">
+					<img src="assets/img/logo-svl-global.svg" alt="" width="170">
+				</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarMain">
+					<div class="navbar-nav ml-auto align-items-sm-center">
+						<a class="nav-item nav-link active" href="index.php">Overview</a>
+						<a class="nav-item nav-link" href="privacy-policy.php">Privacy Policy</a>
+						<a class="nav-item nav-link" href="privacy-policy.php">Terms of Service</a>
+						<a class="nav-item nav-link" href="privacy-policy.php">Technologies</a>
+						<a class="nav-item nav-link" href="privacy-policy.php">FAQ</a>
+						<div class="nav-item dropdown allServicesDropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="allServicesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								<i class="fas fa-th"></i>
+							</a>
+							<div class="dropdown-menu dropdown-menu-right" aria-labelledby="allServicesDropdown">
+								<ul class="nav text-center">
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -345px;"></span>
+											<span class="services-text">Account</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -138px;"></span>
+											<span class="services-text">Search</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -1035px;"></span>
+											<span class="services-text">Maps</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -1449px;"></span>
+											<span class="services-text">YouTube</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -759px;"></span>
+											<span class="services-text">Play</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -2208px;"></span>
+											<span class="services-text">News</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -2139px;"></span>
+											<span class="services-text">Gmail</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -2622px;"></span>
+											<span class="services-text">Contacts</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 0;"></span>
+											<span class="services-text">Drive</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -1794px;"></span>
+											<span class="services-text">Calendar</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -1587px;"></span>
+											<span class="services-text">Translate</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -2070px;"></span>
+											<span class="services-text">Photos</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -69px;"></span>
+											<span class="services-text">Shopping</span>
+										</a>
+									</li>
+									<li class="nav-item col-sm-4">
+										<a href="#" class="nav-link">
+											<span class="services-icon" style="background-position: 0 -1242px;"></span>
+											<span class="services-text">Duo</span>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
 <!--
 Header End
 -->
@@ -71,7 +205,110 @@ Main Content End
 <!--
 Footer Start
 -->
-<?php include 'footer.php';?>
+		<!-- Footer Start -->
+		<footer class="footer-area footer2 py-5">
+		  <div class="container">
+			<div class="row justify-content-between">
+			  <!-- Footer Widget Area-->
+			  <div class="col-12 col-sm-10 col-lg-3">
+				<div class="footer-widget-area mb-5">
+					<a class="d-block mb-4" href="index.php">
+						<img src="assets/img/logo-svl-global-white.svg" alt="">
+					</a>
+				  <p>It's crafted with the latest trend of design &amp; coded with all modern approaches.</p>
+				  <div class="newsletter-form">
+					<form action="#">
+					  <input class="form-control" type="email" placeholder="Enter email &amp; press enter">
+					  <button class="btn d-none" type="submit">Go</button>
+					</form>
+				  </div>
+				  <nav class="footer-social-icon d-flex align-items-center">
+					  <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Facbook"><i class="fab fa-facebook-f"></i></a>
+					  <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Twitter"><i class="fab fa-twitter"></i></a>
+					  <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Instagram"><i class="fab fa-instagram"></i></a>
+					  <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Linkedin"><i class="fab fa-linkedin"></i></a><a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Youtube"><i class="fab fa-youtube"></i></a>
+					</nav>
+				</div>
+			  </div>
+			  <!-- Footer Widget Area-->
+			  <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+				<div class="footer-widget-area mb-5">
+				  <h5 class="widget-title">Important Links</h5>
+				  <ul class="list-unstyled">
+					<li><a href="#" target="_blank">Terms &amp; Conditions</a></li>
+					<li><a href="#" target="_blank">About Licences</a></li>
+					<li><a href="#" target="_blank">Help &amp; Support</a></li>
+					<li><a href="#" target="_blank">Careers</a></li>
+					<li><a href="#" target="_blank">Privacy Policy</a></li>
+					<li><a href="#" target="_blank">Community &amp; Forum</a></li>
+				  </ul>
+				</div>
+			  </div>
+			  <!-- Footer Widget Area-->
+			  <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+				<div class="footer-widget-area mb-5">
+				  <h5 class="widget-title">Our Products</h5>
+				  <ul class="list-unstyled">
+					<li><a href="#" target="_blank">Apland Landing</a></li>
+					<li><a href="#" target="_blank">Ecaps Admin</a></li>
+					<li><a href="#" target="_blank">Bigshop Ecommerce</a></li>
+					<li><a href="#" target="_blank">Classy Multipurpose</a></li>
+					<li><a href="#" target="_blank">Educamp Education</a></li>
+					<li><a href="#" target="_blank">Champ Portfolio</a></li>
+				  </ul>
+				</div>
+			  </div>
+			  <!-- Footer Widget Area-->
+			  <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+				<div class="footer-widget-area mb-5">
+				  <h5 class="widget-title">My Account</h5>
+				  <ul class="list-unstyled">
+					<li><a href="#" target="_blank">Community &amp; Forum</a></li>
+					<li><a href="#" target="_blank">About Licences</a></li>
+					<li><a href="#" target="_blank">Careers</a></li>
+					<li><a href="#" target="_blank">Terms &amp; Conditions</a></li>
+					<li><a href="#" target="_blank">Privacy Policy</a></li>
+					<li><a href="#" target="_blank">Help &amp; Support</a></li>
+				  </ul>
+				</div>
+			  </div>
+			</div>
+		  </div>
+		  <div class="container">
+			<div class="row align-items-center justify-content-between">
+			  <div class="col-12 col-md-6 col-lg-5">
+				<!-- Copywrite Text-->
+				<div class="footer--content-text">
+				  <p class="mb-0">All rights reserved by <a href="#" target="_blank">SVL Global</a></p>
+				</div>
+			  </div>
+			  <div class="col-12 col-md-auto">
+				<!-- Footer Nav-->
+				<nav class="nav footer-nav">
+					<a class="nav-item nav-link" href="#" target="_blank">Privacy Policy</a>
+					<a class="nav-item nav-link" href="#" target="_blank">Terms &amp; Conditions</a>
+					<a class="nav-item nav-link" href="#" target="_blank">Support</a>
+				  </nav>
+			  </div>
+			</div>
+		  </div>
+		</footer>
+		<!-- Footer End -->
+
+		<!--
+		Back To Top
+		-->
+		<a href="#" class="cd-top js-cd-top text-uppercase"><i class="fas fa-chevron-up"></i></a>
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+		<!-- UIkit JS -->
+		<script src="https://cdn.jsdelivr.net/npm/uikit@3.3.3/dist/js/uikit.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/uikit@3.3.3/dist/js/uikit-icons.min.js"></script>
+		<script src="assets/js/back-to-top.js"></script>
+	</body>
+</html>
 <!--
 Footer Start
 -->
